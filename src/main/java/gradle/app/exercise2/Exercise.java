3bf184b60b85e6583c0746f73f02e225f923e484
Ming Wang <ming.wang@luqel-water.com>
2020-04-24 19:56:19 +0800

    valid paren theses

--- src/main/java/gradle/app/exercise2/Exercise.java
+++ src/main/java/gradle/app/exercise2/Exercise.java
@@ -1,6 +1,7 @@
 package gradle.app.exercise2;
 
 import java.io.IOException;
+import java.util.Stack;
 
 public class Exercise {
     public String getTokens(String expression) throws IOException {
@@ -12,4 +13,29 @@ public class Exercise {
         }
         return sb.toString();
     }
+
+    public boolean isValidParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character ch = s.charAt(i);
+            switch (ch) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                default:
+                    if (stack.isEmpty() || stack.pop() != ch) {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+
 }
--- src/test/java/gradle/app/Exercise2Test.java
+++ src/test/java/gradle/app/Exercise2Test.java
@@ -5,10 +5,29 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class Exercise2Test {
     @Test
     public void testGetTokens() throws IOException {
         Exercise e = new Exercise();
         System.out.println(e.getTokens("(5 + 8) * 13"));
+    }
+
+
+    @Test
+    public void testIsValidParentheses() {
+        Exercise e = new Exercise();
+        assertTrue(e.isValidParentheses("((()))"));
+        assertTrue(e.isValidParentheses("[({})]"));
+        assertFalse(e.isValidParentheses("[({(})]"));
+
+        assertFalse(e.isValidParentheses("((("));
+        assertFalse(e.isValidParentheses("((())1)1"));
+        assertFalse(e.isValidParentheses("(1231"));
+        assertFalse(e.isValidParentheses("((123123)1)1"));
+        assertFalse(e.isValidParentheses("((((){{}1)1"));
+        assertFalse(e.isValidParentheses("((()){}{}{}{((())))1"));
     }
 }
