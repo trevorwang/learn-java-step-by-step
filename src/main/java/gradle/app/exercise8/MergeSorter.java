2938936bd2a04766fb146eae73baecbfb097601f
Ming Wang <ming.wang@luqel-water.com>
2020-04-28 20:43:45 +0800

    归并非递归实现

--- src/main/java/gradle/app/exercise8/MergeSorter.java
+++ src/main/java/gradle/app/exercise8/MergeSorter.java
@@ -2,6 +2,8 @@ package gradle.app.exercise8;
 
 import gradle.app.exercise6.Sorter;
 
+import java.util.Arrays;
+
 public class MergeSorter implements Sorter {
     int[] array2;
 
@@ -10,8 +12,26 @@ public class MergeSorter implements Sorter {
         array2 = new int[arr.length];
         int begin = 0;
         int end = arr.length - 1;
-        mergeSort(arr, begin, end);
+        mergeSortLoop(arr);
     }
+
+    private void mergeSortLoop(int[] array) {
+        int len = 1;
+        while (array.length > len) {
+            for (int i = 0; i + len < array.length; i += len * 2) {
+                int left = i;
+                int mid = i + len - 1;
+                int right = i + len * 2 - 1;
+                if (right > array.length - 1) {
+                    right = array.length - 1;
+                }
+                merge(array, left, mid, right);
+            }
+            len <<= 1;
+        }
+
+    }
+
 
     private void mergeSort(int[] arr, int begin, int end) {
         if (begin < end) {
@@ -24,6 +44,7 @@ public class MergeSorter implements Sorter {
 
 
     private void merge(int[] array, int low, int mid, int high) {
+        System.out.println(String.format("merge from : %d,  %d, %d", low, mid, high));
         int i = low, j = mid + 1, k = low;
         while (i <= mid && j <= high) {
             if (array[i] <= array[j])
@@ -42,5 +63,6 @@ public class MergeSorter implements Sorter {
         for (i = low; i <= high; i++) {
             array[i] = array2[i];
         }
+        System.out.println(Arrays.toString(array));
     }
 }
