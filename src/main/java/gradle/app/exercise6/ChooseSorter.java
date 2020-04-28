2451c59ed44b6db4787cf8ffb64f1a7206a8e052
Ming Wang <ming.wang@luqel-water.com>
2020-04-28 13:41:04 +0800

    PriorityQueue选择排序

--- src/main/java/gradle/app/exercise6/ChooseSorter.java
+++ src/main/java/gradle/app/exercise6/ChooseSorter.java
@@ -1,19 +1,30 @@
 package gradle.app.exercise6;
 
+import java.util.PriorityQueue;
+
 public class ChooseSorter implements Sorter {
     @Override
     public void sort(int[] arr) {
-        int povit, pos = -1;
+//        int povit, pos = -1;
+//        for (int i = 0; i < arr.length; i++) {
+//            povit = Integer.MAX_VALUE;
+//            for (int j = i; j < arr.length; j++) {
+//                if (povit > arr[j]) {
+//                    pos = j;
+//                    povit = arr[j];
+//                }
+//            }
+//            arr[pos] = arr[i];
+//            arr[i] = povit;
+//        }
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i : arr) {
+            queue.add(i);
+        }
+
         for (int i = 0; i < arr.length; i++) {
-            povit = Integer.MAX_VALUE;
-            for (int j = i; j < arr.length; j++) {
-                if (povit > arr[j]) {
-                    pos = j;
-                    povit = arr[j];
-                }
-            }
-            arr[pos] = arr[i];
-            arr[i] = povit;
+            arr[i] = queue.remove();
         }
     }
 }
